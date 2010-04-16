53d716fb0e332f30d860bb3d68fc5fd199241a5e
marco singer <msinger@crouwel-2.local>
2010-04-16 17:40:45 -0300

    acertando products controller (route)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,24 +6,4 @@ class ProductsController < ApplicationController
       format.iphone {render :layout => false}
     end
   end
-  
-  def create
-    @product = Product.new(params[:product])
-    if @product.save
-      flash[:notice] = "Successfully created contact."
-      redirect_to admin_path
-    else
-      render new_admin_products_path
-    end
-  end
-  
-  def update
-      @product = Product.find(params[:id])
-      if @product.update_attributes(params[:product])
-        flash[:notice] = "Successfully updated contact."
-        redirect_to admin_path
-      else
-        render edit_admin_product_url(@product)
-      end
-    end
 end
