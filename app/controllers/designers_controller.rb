daac043ba4a95c0b789c108307a59e55c5aec285
marco singer <msinger@crouwel-2.local>
2010-04-22 17:37:38 -0300

    pag designer linkado no banco

--- app/controllers/designers_controller.rb
+++ app/controllers/designers_controller.rb
@@ -1,6 +1,6 @@
 class DesignersController < ApplicationController
-  def show
-    @designer = Designer.by_product(params[:id]).by_language("ptBR").first
+  def index
+    @designer = Designer.by_product(params[:product_id]).by_language("ptBR").first
     
      respond_to do |format|
         format.iphone { render :layout => false }
--- config/initializers/paperclip_tokens.rb
+++ config/initializers/paperclip_tokens.rb
@@ -1,3 +1,3 @@
-Paperclip::Attachment.interpolations[:product_id] = proc do |attachment, style|
+Paperclip::Attachment.interpolation[:product_id] = proc do |attachment, style|
 	attachment.instance.product_id
 end
