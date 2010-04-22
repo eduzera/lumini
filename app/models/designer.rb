4d742f0693f97e2350ed2dcaa480660bc4be8f74
marco singer <msinger@crouwel-2.local>
2010-04-22 13:11:21 -0300

    acesso pag designer via banco

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,4 +1,17 @@
 class Designer < ActiveRecord::Base
   has_one :product
   has_many :lang_designer
+  
+  named_scope :by_product, lambda { |product| {
+                                :joins => [:product],
+                                :conditions => ["products.id = ?", product]}}
+                                
+                                
+  named_scope :by_language, lambda { |language| {
+                                :select => "designers.id 'id', designers.name 'name', lang_designers.biography 'bio', 
+                                            designers.birthdate 'birthdate', designers.deathdate 'deathdate'",
+                                :joins => {:lang_designer => [:language]},
+                                :conditions => ["languages.abbr = ?", language]}}
+                        
 end
+
