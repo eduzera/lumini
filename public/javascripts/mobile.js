6cf693a6cfd22e2114f25bc3501d5c805cfd0e1b
marco singer <msinger@crouwel-2.local>
2010-04-26 13:28:48 -0300

    subindo

--- public/javascripts/mobile.js
+++ public/javascripts/mobile.js
@@ -1,20 +1,18 @@
-$.jQTouch({	
-	statusBar: 'black-translucent',
-				preloadImages: [
-					'themes/jqt/img/chevron_white.png',
-					'themes/jqt/img/bg_row_select.gif',
-					'themes/jqt/img/back_button_clicked.png',
-					'themes/jqt/img/button_clicked.png'
-					],
-				addGlossToIcon: false,
-				slideSelector: 'a',
-				statusBar: 'black',
-				icon: 'appicon.png'
-
-});
 
 $(document).ready(function() 
 {
+	
+	$.jQTouch({	
+		statusBar: 'black-translucent',
+					addGlossToIcon: false,
+					slideSelector: 'a',
+					statusBar: 'black',
+					icon: 'appicon.png'
+
+	});
+	
+	
+	
 	$('.swipe_trigger').live('swipe', function(event, info) {
 		alert(info.direction);
 	});
