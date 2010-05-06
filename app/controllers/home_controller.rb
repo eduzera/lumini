b2507a690c9b5a3fd79f79c44fddf633e8841388
marco singer <msinger@crouwel-2.local>
2010-05-06 20:05:53 -0300

    finalizando solucao

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,17 @@ class HomeController < ApplicationController
  
   def index
     session[:language] = 'pt_BR' if session[:language].nil?
+    
     @languages = Language.all
     @cover = Cover.active
     @image = Image.by_imageable_type("Product").by_image_type("fotografia").by_cover.by_order
+    
+    @solution = Solution.all_with_filter.by_language(session[:language])
+    unless @solution.empty?
+       @solution = @solution.find(@cover.first.solution_id)
+    else
+      @solution=nil
+    end
     
     respond_to do |format|
       format.iphone
--- app/models/cover.rb
+++ app/models/cover.rb
@@ -2,7 +2,7 @@ class Cover < ActiveRecord::Base
   belongs_to :solution
   has_many   :cover_element
   
-  named_scope :active,  :select => "products.id 'id', lang_products.name 'product_name', cover_elements.grid_size 'product_grid'", 
+  named_scope :active,  :select => "products.id 'id', covers.solution_id 'solution_id', lang_products.name 'product_name', cover_elements.grid_size 'product_grid'", 
                         :joins => {:cover_element => {:product => [:lang_product]}}
   
 end
