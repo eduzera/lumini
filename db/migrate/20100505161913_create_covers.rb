367e60bc9908e5457076e70cc2b8348bd9709d13
eduardo zaghi <ezaghi@magalhaes.aquinanu.com.br>
2010-05-06 18:23:26 +0200

    mudanca do cover

--- db/migrate/20100505161913_create_covers.rb
+++ db/migrate/20100505161913_create_covers.rb
@@ -2,6 +2,8 @@ class CreateCovers < ActiveRecord::Migration
   def self.up
     create_table :covers do |t|
       t.references :solution
+      t.datetime :public_date
+      t.boolean  :status , :defaults => false
       t.timestamps
     end
   end
